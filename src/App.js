95daba73b235ea49548212d25192f2ef97efca58
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-12-20 13:12:39 +0600

    added menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './componants/Home/Home';
+import AddUser from './componants/AddUser/AddUser';
+import UpdateUser from './componants/UpdateUser/UpdateUser';
+import Users from './componants/Users/Users';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<AddUser />} />
+          <Route path="/register" element={<Users />} />
+          <Route path="/register" element={<UpdateUser />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
