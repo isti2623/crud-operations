6e2c5706efbaa185fc1c756f544ff0db15e9536d
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-12-20 13:14:39 +0600

    header

--- src/componants/Header/Header.js
+++ src/componants/Header/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-
+        <div className="header">
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/users">Users</Link>
+                <Link to="/users/add">Add User</Link>
+            </nav>
         </div>
     );
 };
